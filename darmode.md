c1e237f36483b6255d6b5dc2f46ac8844c72d966
ErnieBuyco <buycoernie@gmail.com>
2020-03-18 03:00:02 +0800

    test_develop

---
+++ darmode.md
@@ -0,0 +1,5 @@
+https://medium.com/@rossbulat/react-dark-mode-with-styled-theming-and-context-57557de6400
+https://github.com/rossbulat/react-theming-dark-mode
+https://medium.com/maxime-heckel/switching-off-the-lights-adding-dark-mode-to-your-react-app-with-context-and-hooks-f41da6e07269
+https://dev.to/smakosh/how-to-add-dark-mode-easily-with-a-custom-react-hook-10mj
+https://alterclass.hashnode.dev/how-to-add-dark-mode-to-react-with-context-and-hooks-ck77np7gp08cid9s1kcn5kyp9
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+function useStats(){
+    const [stats, setStats] = useState();
+    useEffect( () => {
+
+        async function fetchData(){
+            console.log('Fetching data');
+            const data = await fetch('https://covid19.mathdro.id/api').then(
+                res => res.json()
+            )
+            setStats(data);
+        }
+       fetchData();
+    },[]);
+    return stats;
+}
+
+function Stats(){
+    const stats = useStats();
+    if(!stats) return <p>Loading ....</p>
+    console.log(stats)
+    return(
+      <>
+              <div className="static__block">
+            <h3>Confirmed:</h3>
+    <span>{stats.confirmed.value}</span>
+        </div>
+         <div className="static__block">
+         <h3>Deaths:</h3>
+ <span>{stats.deaths.value}</span>
+     </div>
+      <div className="static__block">
+      <h3>Recovered:</h3>
+<span>{stats.recovered.value}</span>
+  </div>
+
+      </>
+    )
+}
+export default function IndexPage(){
+    return (
+        <Stats></Stats>
+    )
+}
